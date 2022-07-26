74b4b3cb233276f9adee85a7309400a2cf43128d
shokoohi <hassan.shokoohimatin@gmail.com>
2022-07-26 12:53:52 +0430

    initial commit

---
+++ src/main/java/ir/asta/warehouseone/WarehouseoneApplication.java
@@ -0,0 +1,13 @@
+package ir.asta.warehouseone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/asta/warehouseone/WarehouseoneApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.asta.warehouseone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

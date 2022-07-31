17273f0649832055ea73ff191e62a9441f39f1cb
asus <hassan.shokoohimatin@gmail.com>
2022-07-31 21:13:13 +0430

    index.html set as home page

--- src/main/java/ir/asta/warehouseone/config/JerseyConfig.java
+++ src/main/java/ir/asta/warehouseone/config/JerseyConfig.java
@@ -1,12 +1,21 @@
 package ir.asta.warehouseone.config;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletProperties;
 import org.springframework.context.annotation.Configuration;
+
+import javax.xml.ws.Endpoint;
 
 @Configuration
 public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig(){
         packages("ir.asta.warehouseone.service");
+
+        register(Endpoint.class);
+        property(ServletProperties.FILTER_FORWARD_ON_404, true);
     }
 }
+
+
+
